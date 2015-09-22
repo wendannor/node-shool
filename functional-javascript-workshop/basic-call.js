d0000540d5efebae130c0b1a284ea9cfb4e33dbb
Wendannor <jordan.sarre@gmail.com>
2015-09-22 13:43:24 +0200

    Basic call

---
+++ functional-javascript-workshop/basic-call.js
@@ -0,0 +1,7 @@
+function duckCount() {
+  return Array.prototype.slice.call(arguments).filter(function (item) {
+    return Object.prototype.hasOwnProperty.call(item, 'quack');
+  }).length;
+}
+
+module.exports = duckCount;
