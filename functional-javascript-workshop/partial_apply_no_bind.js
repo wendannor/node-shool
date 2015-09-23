ffd09c8fe312eab6db0175f4f8e6bf4da48544ff
Wendannor <jordan.sarre@gmail.com>
2015-09-23 13:41:47 +0200

    Partial apply without bind

---
+++ functional-javascript-workshop/partial_apply_no_bind.js
@@ -0,0 +1,9 @@
+var slice = Array.prototype.slice
+
+ function logger(namespace) {
+   return function () {
+     console.log.apply(null, [namespace].concat(Array.prototype.slice.call(arguments)));
+   };
+ }
+
+ module.exports = logger
